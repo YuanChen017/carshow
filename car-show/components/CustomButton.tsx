ccc505073df91ad1fcf586929729c42ad2ed696f
YuanChen <yuc017@ucsd.edu>
2023-09-13 22:32:33 -0700

    added navbar and footer

--- car-show/components/CustomButton.tsx
+++ car-show/components/CustomButton.tsx
@@ -1,12 +1,22 @@
-"use client"
-import { CustomButtonProps } from '@/types'
-import React from 'react'
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
-    return (
-        <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
-            <span className={"flex-1"}>{title}</span>
-        </button>
-    )
-}
+"use client";
+import { CustomButtonProps } from "@/types";
+import React from "react";
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={"flex-1"}>{title}</span>
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
--- car-show/types/index.ts
+++ car-show/types/index.ts
@@ -1,7 +1,8 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps{
-    title:string;
-    containerStyles?:string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "submit" | "button";
 }
